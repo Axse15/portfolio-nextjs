de759cc198b7a28d8457cfeb3e7819f08d68bbe7
Axse15 <fazliadri11.com>
2026-07-24 19:11:48 +0700

    Add contact form with email feature

--- data/timeline.ts
+++ data/timeline.ts
@@ -2,15 +2,39 @@ import { TimelineItem } from "@/types";
 
 export const timeline: TimelineItem[] = [
   {
+    year: "2020",
+    title: "Masuk SMK Multimedia Nusantara Jakarta",
+    subtitle: "SMK Multimedia Nusantara Jakarta",
+    description:
+      "Memulai dari SMK Multimedia Nusantara Jakarta ini saya mulai mengenal tentang coding.",
+    color: "from-cyan-500 to-blue-500",
+  },
+  {
     year: "2023",
     title: "Mahasiswa Teknologi Informasi",
     subtitle: "Universitas Bina Sarana Informatika",
     description:
-      "Memulai perjalanan sebagai mahasiswa Teknologi Informasi dan mempelajari dasar-dasar pemrograman, basis data, serta pengembangan aplikasi.",
+      "Memulai perjalanan sebagai mahasiswa Teknologi Informasi dengan mengambil jurusan Sistem Informasi dan mempelajari dasar-dasar pemrograman, basis data, serta pengembangan aplikasi.",
     color: "from-cyan-500 to-blue-500",
   },
   {
-    year: "2024",
+    year: "2025",
+    title: "Pengembangan FinCycle",
+    subtitle: "Laravel 13",
+    description:
+      "Mengembangkan website untuk mengelola keuangan pribadi yang menjadi tugas untuk kegiatan IT BootCamp yang diadakan oleh pihak kampus.",
+    color: "from-purple-500 to-pink-500",
+  },
+  {
+    year: "2025",
+    title: "Pengembangan Aplikasi Registrasi Pernikahan",
+    subtitle: "Laravel 13",
+    description:
+      "Mengembangkan Aplikasi Registrasi Pernikahan menggunakan framework flutter untuk kebutuhan tugas Mobile Programming.",
+    color: "from-purple-500 to-pink-500",
+  },
+  {
+    year: "2026",
     title: "Pengembangan PECOR",
     subtitle: "Laravel 13",
     description:
@@ -18,7 +42,7 @@ export const timeline: TimelineItem[] = [
     color: "from-purple-500 to-pink-500",
   },
   {
-    year: "2025",
+    year: "2026",
     title: "Smart RT",
     subtitle: "Laravel 13",
     description:
